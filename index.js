8804f6c93a4d960cc28efdcdd448dcebb269c856
callixmiller <calmill413@gmail.com>
2023-01-10 16:24:20 -0500

    index.js passing all tests

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400) {
+    return 'This one is on me!';
+  } else if (value > 400 && value <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (value > 2000 && value <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(place){
+  return (place === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+        return 'Bye.';
+  }
 }
